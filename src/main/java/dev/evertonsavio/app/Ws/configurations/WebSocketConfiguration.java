192ea0b40b0b3d82e73e6837ce927cbf8f058584
Everton Savio Santos Lucas <everluca@outlook.com>
2020-11-26 10:23:40 -0300

    :sparkles: chat websocket methods

--- src/main/java/dev/evertonsavio/app/Ws/configurations/WebSocketConfiguration.java
+++ src/main/java/dev/evertonsavio/app/Ws/configurations/WebSocketConfiguration.java
@@ -1,6 +1,9 @@
 package dev.evertonsavio.app.Ws.configurations;
 
+import dev.evertonsavio.app.Ws.handler.ChatWebsocketHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -9,9 +12,17 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfiguration implements WebSocketConfigurer {
 
+    public static final String CHAT_ENDPOINT = "/chat";
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        
+        webSocketHandlerRegistry.addHandler(getChatWebSocketHandler(), CHAT_ENDPOINT)
+                .setAllowedOrigins("*");
     }
+
+    @Bean
+    public WebSocketHandler getChatWebSocketHandler(){
+        return new ChatWebsocketHandler();
+    }
+
 }
