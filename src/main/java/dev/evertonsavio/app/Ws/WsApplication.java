2562820ee82a7dcc88309b885b2239abfc03a18d
Everton Savio Santos Lucas <everluca@outlook.com>
2020-11-26 10:07:42 -0300

    :tada: vanilla websocket

---
+++ src/main/java/dev/evertonsavio/app/Ws/WsApplication.java
@@ -0,0 +1,13 @@
+package dev.evertonsavio.app.Ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/evertonsavio/app/Ws/WsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.evertonsavio.app.Ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
